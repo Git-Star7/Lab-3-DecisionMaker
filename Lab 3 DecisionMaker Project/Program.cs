68fd93ba359a705c371eeedf531834e800d3f8e0
Michael <Michael@acdmail.com>
2020-01-22 16:01:55 -0500

    first commit

---
+++ Lab 3 DecisionMaker Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab_3_DecisionMaker_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
